3c015ef67d4e8a136fd42b8aa80430bf9aac4be0
selpathor <mcrichton262@gmail.com>
2019-09-29 01:07:48 -0400

    Added basic movement. Moved events inside the frame locking IF.

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ class ZeldaGame:
 		# self.screen = pygame.display.set_mode(self.size)
 		self.screen = screenSize(width,height)
 		self.clock = pygame.time.Clock()
+		nextFrame = clock()
+		frame = 0
 
 		self.player = Player("Sprites/links.gif",32)
 
@@ -28,44 +30,63 @@ class ZeldaGame:
 		moveLeft = False
 
 		while running:
-			for event in pygame.event.get():
 
-				if event.type == pygame.QUIT:
-					sys.exit(0)
+			if clock() > nextFrame:
+				frame += 1
+				nextFrame += 60
+				for event in pygame.event.get():
 
-				elif event.type == pygame.KEYDOWN:
-					print("DOWN",event.key)
-					if event.key == pygame.K_ESCAPE:
+					if event.type == pygame.QUIT:
 						sys.exit(0)
-					elif event.key == pygame.K_d:
-						moveRight = True
-					elif event.key == pygame.K_a:
-						moveLeft = True
-					elif event.key == pygame.K_w:
-						moveUp = True
-					elif event.key == pygame.K_s:
-						moveDown = True
 
-				elif event.type == pygame.KEYUP:
-					print("UP",event.key)
-					if event.key == pygame.K_d:
-						moveRight = False
-					elif event.key == pygame.K_a:
-						moveLeft = False
-					elif event.key == pygame.K_w:
-						moveUp = False
-					elif event.key == pygame.K_s:
-						moveDown = False
+					elif event.type == pygame.KEYDOWN:
+						print("DOWN",event.key)
+						if event.key == pygame.K_ESCAPE:
+							sys.exit(0)
+						elif event.key == pygame.K_d:
+							moveRight = True
+						elif event.key == pygame.K_a:
+							moveLeft = True
+						elif event.key == pygame.K_w:
+							moveUp = True
+						elif event.key == pygame.K_s:
+							moveDown = True
 
+					elif event.type == pygame.KEYUP:
+						print("UP",event.key)
+						if event.key == pygame.K_d:
+							moveRight = False
+							self.player.animationDirection(0,0)
+						elif event.key == pygame.K_a:
+							moveLeft = False
+							self.player.animationDirection(2,0)
+						elif event.key == pygame.K_w:
+							moveUp = False
+							self.player.animationDirection(3,0)
+						elif event.key == pygame.K_s:
+							moveDown = False
+							self.player.animationDirection(1,0)
 
-			if moveUp == True:
+				#Player Animation
+				if moveUp == True:
+					self.player.animationDirection(3,frame)
+				elif moveDown == True:
+					self.player.animationDirection(1,frame)
+				elif moveRight == True:
+					self.player.animationDirection(0,frame)
+				elif moveLeft == True:
+					self.player.animationDirection(2,frame)
 
-			elif moveDown == True:
+				#Player Movement
+				if moveUp == True:
+					self.player.moveDirection(3,5)
+				elif moveDown == True:
+					self.player.moveDirection(1,5)
+				if moveRight == True:
+					self.player.moveDirection(0,5)
+				elif moveLeft == True:
+					self.player.moveDirection(2,5)
 
-			if moveRight == True:
-
-			elif moveLeft == True:
-				
 
 
 if __name__ == '__main__':
--- player.py
+++ player.py
@@ -11,3 +11,23 @@ class Player(pygame.sprite.Sprite):
 		transformSprite(self.sprite,0, .5)
 		showSprite(self.sprite)
 
+	#0=right, 1=down, 2=left, 3=up
+	def animationDirection(self, direction ,frame):
+		print(frame,frame%8)
+		changeSpriteImage(self.sprite, direction*8+(frame%8) )
+
+	def moveDirection(self, direction, speed):
+		center = self.sprite.rect.center
+		print(center)
+		#Right
+		if direction == 0:
+			moveSprite(self.sprite, center[0]+speed, center[1], True)
+		#Down
+		elif direction == 1:
+			moveSprite(self.sprite, center[0], center[1]+speed, True)
+		#Left
+		elif direction == 2:
+			moveSprite(self.sprite, center[0]-speed, center[1], True)
+		#Up
+		elif direction == 3:
+			moveSprite(self.sprite, center[0], center[1]-speed, True)
